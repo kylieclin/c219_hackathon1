8cffebd1bb6b2addaf6b14481ace7cce0d5c465c
Caroline Pan <abc1588@yahoo.com>
2019-03-06 15:49:47 -0800

    added dispenser.js - CP

---
+++ dispenser.js
@@ -0,0 +1,70 @@
+
+class Dispenser{
+    constructor( numberOfRows, clickCallback ){
+        this.rowCount = numberOfRows;
+        this.rows = [];
+        //this.activeRow = [];
+        this.marbleArray = [];
+        this.domElements = {dispenser:null};
+        this.handleMarbleClick = this.handleMarbleClick.bind( this );
+    }
+    handleMarbleClick(){
+        console.log('marble was clicked');
+        this.callbacks.click( this );
+    }
+
+    createRow(){
+        // create new row object, for each new row to add:
+
+        // generate an array of marbles with random colors
+        this.marbleArray = ['blue','yellow','red','black','blue','yellow','red','black'];
+
+        for (var row_index=0; row_index < this.rowCount; row_index++){
+            var currRow = new Row( this.marbleArray );
+            this.rows.push(currRow);
+            var rowDomElement = currRow.render();
+            $('.board').append(rowDomElement);
+        }
+    }
+
+    addMarbleToRow(){
+
+    }
+    removeMarbleFromRow(){
+
+    }
+    addRowToDispenser(){
+
+
+    }
+
+    render(){
+        this.domElements.dispenser = $("<div>",{
+            'class': 'board'
+        });
+        this.domElements.dispenser.click( this.handleClick );
+
+        return this.domElements.dispenser;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
