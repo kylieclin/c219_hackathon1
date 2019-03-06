46e50d5fe4da21792a63c878affdea7aacf8c527
Kylie Lin <kylieclin@gmail.com>
2019-03-06 12:45:40 -0800

    potion for play1 added - not randomize yet

---
+++ player.js
@@ -0,0 +1,96 @@
+//game (potionobj, marbleobj)
+class Game{
+    constructor(){
+        // this.marble;
+        // this.potion;
+        // this.score;
+        // this.marbles =  new Marbles();
+        this.potion = new Potion();
+    }
+    //click (event listener)
+    addEventListener(){
+        //.click to pick the potion
+        //.click to pick marbles
+        //.click to move the marbles
+    }    
+    
+    //1)generate marbles
+    generateMarble(){
+        //get the marble datas from main.js 
+        //pass into class marble
+    
+    }
+    //2)generate potion
+       
+    generatePotion(potionData){
+        this.potion.createPotion(potionData);
+        //get the potion datas from main.js and pass into class "______"?
+    }
+
+    
+    //3)change player
+        //define whos turn to play (even/odd or boolean)
+    changePlayer(){
+        if(firstplayer){
+            //firstplayer play
+            firstplayer = true;
+        } else {
+            //secondplayer play
+            firstplayer = false;
+        }
+    }
+    
+    //4)pick/show potions
+        //let player to pick potions?
+        //place the potion to the player's box
+    pickPotion(){
+        //player take turns pick potion
+        //click to choose
+    }
+    //5)pick marbles
+    pickMarble(){
+        //pick marbles
+        //put the marbles in the marble container
+    }
+    
+    //6)show picked/earned marbles
+    renderPickedMarbles(){
+        //get total marbles from the collected marble container
+        //get mouse position
+        //call the get marbles function
+        //move the marbles as call back
+        //give up the marble
+    }
+    
+    //7)move marbles
+        //flusk that holds three marbles
+        //get the current potion data and detail for potions that you can hold in
+        //move marbles from the marble comtainer to the player's panel   
+    moveMarble(){
+        //get the mouse poistion
+        //first click select the marble, second click move the marble
+    }
+    
+    //8)check player's potion
+        // reveal the portion once all the marbles are collect
+        //add scores
+        //and change player after done
+    checkPotion(){
+        //if potion is full, flip
+    }
+    
+    winGame(){
+        //win the game
+        //render status (count score)
+        //default set to if any potion is full?
+    }
+    
+    //reset(extra)
+}
+
+
+
+
+
+
+
---
+++ startApp_main.js
@@ -0,0 +1,16 @@
+$(document).ready(startApp);
+
+var newGame;
+var potion ={
+    color: ['red', 'blue', 'yellow'],//, 'black'],
+    numbers: [1,2,3]
+}
+
+function startApp(){
+    debugger;
+    // var gameObj = { //all the marbles + // all the potion  }
+    newGame = new Game();
+    newGame.generatePotion(potion);
+
+}
+
