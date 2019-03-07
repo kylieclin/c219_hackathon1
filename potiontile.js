ba196e8b625044f71aaf4c51f16af601b2ca05a9
jenOng19 <jennifer.sy.ong@gmail.com>
2019-03-06 16:01:01 -0800

    recreated render potion function

--- potiontile.js
+++ potiontile.js
@@ -1,35 +1,45 @@
 class Potion{
-    constructor(){
-        this.potionInfo=null;
-        this.playerOne=null;
-
-        
-
-        this.createPotion = this.createPotion.bind(this);
-
-    }
-    createPotion(potionInfo){
+    constructor(potionInfo, player){
         this.potionInfo=potionInfo;
-        this.potionInfo.color[0];
-        debugger;
+        this.potionDom={
+            potionStorage:{
+                color:[],
+                numbers:[]
+            }
+        };
+        this.marbleCollect=['red', 'blue', 'blue'];
+        this.renderPotion = this.renderPotion.bind(this);
+    }
+    renderPotion(){
         for(var pIndex = 0; pIndex < this.potionInfo.color.length; pIndex++){
-            this.playerOne=$('<div>',{
+            this.potionDom=$('<div>',{
             'css':{
                 'background-color': this.potionInfo.color[pIndex],
             },
 
             'text':this.potionInfo.numbers[pIndex],
             'class': 'potionbala ' + this.potionInfo.color[pIndex] + this.potionInfo.numbers[pIndex]
-           
+        
         })
-        this.render();
+        var potionContainer=$('<div>').addClass('potionContainer');
+        potionContainer.append(this.potionDom);
+        this.potionDom.potionStorage.color.push(this.potionInfo.color[pIndex]);
+        this.potionDom.potionStorage.numbers.push(this.potionInfo.numbers[pIndex])
+        };
+
+        
+        return potionContainer;
         }
         
-        
-        
-    }
-    render(){
-        var potionContainer=$('.player1-has');
-        potionContainer.append(this.playerOne)
-    }
+        checkFilledStatus(){
+            debugger;
+            var potionNum= this.potionDom.potionStorage.numbers;
+            var filled;
+            for(var numIndex=0; numIndex<potionNum.length; numIndex++){
+                if(potionNum[numIndex] != 0){
+                    return filled = false;
+                }
+            }
+            return filled = true;
+        }
 }
